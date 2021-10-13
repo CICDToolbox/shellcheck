824e855f880e223352893e175e05bb2a74688e93
Tim Gurney aka Wolf <wolf@tgwolf.com>
2021-10-13 14:35:25 +0100

    update

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -9,6 +9,8 @@ This changelog was automatically generated using [Caretaker](https://github.com/
 
 - update [`[head]`](https://github.com/CICDToolbox/shellcheck/commit/)
 
+- update [`[cc24c87]`](https://github.com/CICDToolbox/shellcheck/commit/cc24c8723434cc463e1428163f8c4c1a40aec319)
+
 - update [`[0f36579]`](https://github.com/CICDToolbox/shellcheck/commit/0f365790bdc0df9a7b1b0565dc145fd11751ed9f)
 
 - update [`[02d2e1b]`](https://github.com/CICDToolbox/shellcheck/commit/02d2e1b0bdbf1ed97f2a0f0a656a182a3ddbb082)
--- pipeline.sh
+++ pipeline.sh
@@ -28,7 +28,7 @@ set -Eeuo pipefail
 #wget -q -O - "http://wordpress.org/latest.tar.gz" | tar -xzf - -C /var/www
 
 #INSTALL_COMMANDS=('wget -qO- "https://github.com/koalaman/shellcheck/releases/download/stable/shellcheck-stable.linux.x86_64.tar.xz" | tar -xzf' 'sudo cp "shellcheck-stable/shellcheck" /usr/bin/' 'sudo chmod 755 /usr/bin/shellcheck')
-INSTALL_COMMAND='wget -q -O - "https://github.com/koalaman/shellcheck/releases/download/stable/shellcheck-stable.linux.x86_64.tar.xz | tar -xzf - -C /usr/bin/'
+INSTALL_COMMAND='wget -q -O - "https://github.com/koalaman/shellcheck/releases/download/stable/shellcheck-stable.linux.x86_64.tar.xz" | tar -xzf - -C /usr/bin/'
 #('wget -qO- "https://github.com/koalaman/shellcheck/releases/download/stable/shellcheck-stable.linux.x86_64.tar.xz" | tar -xzf' 'sudo cp "shellcheck-stable/shellcheck" /usr/bin/' 'sudo chmod 755 /usr/bin/shellcheck')
 
 TEST_COMMAND='shellcheck'
